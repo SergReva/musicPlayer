19cdece11d0edab7bfdbfb6abec5ccdffc712425
SergReva <51054547+SergReva@users.noreply.github.com>
2022-04-15 23:21:43 +0300

    del

--- test.php
+++ test.php
@@ -1,58 +1,38 @@
 <?php
 
     $files = file_get_contents("track.json");
-
     $fileList = [];
     $domain = "http://chevida.at.ua/music/";
     $id = 4;
     $idT = 0;
-    
     $nIndex = 0;
 
-
-//перебор по строчно из файла в той же директории
-    // $text = fopen("1.txt", "r");
-    // $ar = null;
-    // if ($text) {
-    //     while (($buffer = fgets($text)) !== false) {
-    //         $ar[] = $buffer;
-    //     }
-    // }
-    // fclose($text);
-     
-    // print_r($ar);
-//
-
     function translit($str) {
-    $rus = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '&');
-    $lat = array('A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', '', '', '', 'e', 'yu', 'ya', 'and');
-    return str_replace($rus, $lat, $str);
+        $rus = array('А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ё', 'Ж', 'З', 'И', 'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С', 'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ', 'Ы', 'Ь', 'Э', 'Ю', 'Я', 'а', 'б', 'в', 'г', 'д', 'е', 'ё', 'ж', 'з', 'и', 'й', 'к', 'л', 'м', 'н', 'о', 'п', 'р', 'с', 'т', 'у', 'ф', 'х', 'ц', 'ч', 'ш', 'щ', 'ъ', 'ы', 'ь', 'э', 'ю', 'я', '&');
+        
+        $lat = array('A', 'B', 'V', 'G', 'D', 'E', 'E', 'Gh', 'Z', 'I', 'Y', 'K', 'L', 'M', 'N', 'O', 'P', 'R', 'S', 'T', 'U', 'F', 'H', 'C', 'Ch', 'Sh', 'Sch', 'Y', 'Y', 'Y', 'E', 'Yu', 'Ya', 'a', 'b', 'v', 'g', 'd', 'e', 'e', 'gh', 'z', 'i', 'y', 'k', 'l', 'm', 'n', 'o', 'p', 'r', 's', 't', 'u', 'f', 'h', 'c', 'ch', 'sh', 'sch', '', '', '', 'e', 'yu', 'ya', 'and');
+        
+        return str_replace($rus, $lat, $str);
   }
 
     foreach (glob("audio/*.mp3") as $filename) {
         $strArray = ["(", ")","—", "kissvk.com-", "(zvukoff.ru)", "(zf.fm)", ","];
         $title = basename($filename, ".mp3");
-        // $new = "audio/" . '0' . $idT . '1' . ".mp3";
         $songWidth = mb_strimwidth(str_ireplace($strArray, "-", str_ireplace(" ", "_", $filename)), 0, 50, ".mp3");
 
         copy($filename, "audio/back/" . basename($filename));
         rename($filename, translit(trim($songWidth), "-"));
-        // rename($filename, $songWidth);
-        // $nName = glob($new);
 
-        
-        // $idT++;
-    
-            $id++;
-            $val = array(
-                'id' => $id,
-                'class' => 'theDeli',
-                'url' => $domain . translit($songWidth),
-                'author' => 'MUSIC',
-                'title' => str_ireplace("_", " ", $title),
-            );
+        $id++;
+        $val = array(
+            'id' => $id,
+            'class' => 'theDeli',
+            'url' => $domain . translit($songWidth),
+            'author' => 'MUSIC',
+            'title' => str_ireplace("_", " ", $title),
+        );
 
-            array_push($fileList, $val);    
+        array_push($fileList, $val);    
     }
     shuffle($fileList);
 
@@ -61,4 +41,3 @@
     file_put_contents("track.json", $json);
 
 ?>
-
